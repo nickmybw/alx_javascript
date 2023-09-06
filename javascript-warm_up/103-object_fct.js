8251c228bb1ee588cad47b54e2cc8db92b6bfb07
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-06 13:23:18 +0300

    first commit

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+const myObject = {
+  type: "object",
+  value: 12,
+  incr: function () {
+    this.value++; // Increment the value property by 1
+  },
+};
+
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
