27451310be7dc17c8163d88b8c46af2d8d7f3f4e
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-08 16:42:59 +0300

    New update

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,19 +1,16 @@
-const r1 = new Rectangle(2, 3);
-console.log(r1); // Rectangle { width: 2, height: 3 }
-console.log(r1.width); // 2
-console.log(r1.height); // 3
+const Rectangle = require("./2-rectangle");
 
-const r2 = new Rectangle(2, -3);
-console.log(r2); // {}
-console.log(r2.width); // undefined
-console.log(r2.height); // undefined
+const r1 = new Rectangle(3, 3);
+r1.print(); // Instance width: 3 - height: 3
 
-const r3 = new Rectangle(2);
-console.log(r3); // {}
-console.log(r3.width); // undefined
-console.log(r3.height); // undefined
+const r2 = new Rectangle(3, -3);
+console.log(r2); // Instance width: undefined - height: undefined
 
-const r4 = new Rectangle(2, 0);
-console.log(r4); // {}
-console.log(r4.width); // undefined
-console.log(r4.height); // undefined
+const r3 = new Rectangle(3);
+r3.print(); // Instance width: 3
+
+const r4 = new Rectangle(0, 3);
+console.log(r4); // Instance width: undefined - height: undefined
+
+const r5 = new Rectangle();
+console.log(r5); // Instance no argument
--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,19 @@
+#!/usr/bin/node
+
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0) {
-      // create an empty object
-      return {};
+      return {}; // create an empty object
     }
     this.width = w;
     this.height = h;
   }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log("X".repeat(this.width));
+    }
+  }
 }
+
+module.exports = Rectangle;
