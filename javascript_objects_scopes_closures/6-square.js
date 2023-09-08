70ad67f7907c86c4ff42be51ee3b6b3d6b4c8499
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-08 17:32:21 +0300

    first commit

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+const ParentSquare = require("./5-square");
+
+class Square extends ParentSquare {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (typeof c === "undefined") {
+      c = "X";
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
