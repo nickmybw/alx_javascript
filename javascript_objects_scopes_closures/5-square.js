3e337c59bdadfb6f5b92afb80e874ffdd1e92c38
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-08 17:10:15 +0300

    first commit

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,7 @@
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size); // Call the Rectangle constructor with size for both width and height
+  }
+}
+
+module.exports = Square;
