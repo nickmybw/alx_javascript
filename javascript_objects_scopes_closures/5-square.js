73d880fbe99e3dd336edc3db8415574c55fcd681
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-08 17:14:16 +0300

    new update

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
+const Rectangle = require("./4-rectangle");
+
 class Square extends Rectangle {
   constructor(size) {
-    super(size, size); // Call the Rectangle constructor with size for both width and height
+    super(size, size);
   }
 }
-
-module.exports = Square;
