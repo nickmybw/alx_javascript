5ae9652914a43fd015cbe0452ba106819376711f
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-08 16:46:59 +0300

    newest update

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,16 +2,14 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {}; // create an empty object
-    }
-    this.width = w;
-    this.height = h;
-  }
-
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log("X".repeat(this.width));
+    if (w <= 0 || h <= 0 || typeof w !== "number" || typeof h !== "number") {
+      // If w or h is invalid, create an empty object
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      // Otherwise, initialize width and height
+      this.width = w;
+      this.height = h;
     }
   }
 }
