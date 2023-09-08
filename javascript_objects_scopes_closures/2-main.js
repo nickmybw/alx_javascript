e748dd88c91b50ef1061d847fc3899017c2d210a
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-08 16:36:49 +0300

    Update

--- javascript_objects_scopes_closures/2-main.js
+++ javascript_objects_scopes_closures/2-main.js
@@ -1,21 +1,19 @@
-const Rectangle = require("./2-rectangle");
-
 const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+console.log(r1); // Rectangle { width: 2, height: 3 }
+console.log(r1.width); // 2
+console.log(r1.height); // 3
 
 const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
+console.log(r2); // {}
+console.log(r2.width); // undefined
+console.log(r2.height); // undefined
 
 const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+console.log(r3); // {}
+console.log(r3.width); // undefined
+console.log(r3.height); // undefined
 
 const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
+console.log(r4); // {}
+console.log(r4.width); // undefined
+console.log(r4.height); // undefined
--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,10 @@
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== "number" || typeof h !== "number") {
-      // If w or h is invalid, create an empty object
-      this.width = undefined;
-      this.height = undefined;
-    } else {
-      // Otherwise, initialize width and height
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0) {
+      // create an empty object
+      return {};
     }
+    this.width = w;
+    this.height = h;
   }
 }
-
-module.exports = Rectangle;
