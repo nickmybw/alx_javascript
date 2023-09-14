d5d46e0c801eba41b4c6df6614d41027da9c19f8
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-14 18:36:20 +0300

    new commit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,28 +1,21 @@
-#!/usr/bin/node
 const request = require("request");
 const fs = require("fs");
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-    process.exit(1);
+    return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status Code ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  // Write the response body to the specified file
-  fs.writeFile(filePath, body, { encoding: "utf-8" }, (writeError) => {
-    if (writeError) {
-      console.error(writeError);
-      process.exit(1);
+  fs.writeFile(filePath, body, "utf-8", function (error) {
+    if (error) {
+      console.error(error);
+      return;
     }
 
-    console.log(`Successfully saved the content to ${filePath}`);
+    console.log(`File saved to ${filePath}`);
   });
 });
