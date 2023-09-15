ee5d04ab0d97679df5c12c196b6735513da1c2c5
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-15 15:44:07 +0300

    first commit

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,17 +1,26 @@
-const request = require("request");
+const request = require('request');
 
+// Get the API URL from the command line argument
 const apiUrl = process.argv[2];
 
-request(apiUrl, function (error, response, body) {
+// Character ID for Wedge Antilles
+const characterId = 18;
+
+// Send a GET request to the Star Wars API
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
     return;
   }
 
-  const movies = JSON.parse(body).results;
-  const wedgeMovies = movies.filter((movie) =>
-    movie.characters.includes("https://swapi-api.alx-tools.com/api/people/18/"),
+  // Parse the JSON response
+  const data = JSON.parse(body);
+
+  // Filter movies that include Wedge Antilles
+  const moviesWithWedgeAntilles = data.results.filter(movie =>
+    movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
   );
 
-  console.log(wedgeMovies.length);
+  // Print the count of movies with Wedge Antilles
+  console.log(moviesWithWedgeAntilles.length);
 });
