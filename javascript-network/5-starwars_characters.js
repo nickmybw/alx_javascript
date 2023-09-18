c4b5e1ad2f4b7a2ee1c35af67ee26ebb67f29e71
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-09-18 11:45:36 +0300

    second commit

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,27 +1,53 @@
 const request = require("request");
 
+if (process.argv.length !== 3) {
+  console.error("Usage: node 5-starwars_characters.js <Movie_ID>");
+  process.exit(1);
+}
+
 const movieId = process.argv[2];
 
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, function (error, response, body) {
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
+    console.error(`Error requesting API: ${error.message}`);
+    process.exit(1);
   }
 
-  const movie = JSON.parse(body);
+  if (response.statusCode !== 200) {
+    console.error(`Request failed with status code ${response.statusCode}`);
+    process.exit(1);
+  }
 
-  movie.characters.forEach(function (characterUrl) {
-    request(characterUrl, function (error, response, body) {
-      if (error) {
-        console.error(error);
-        return;
-      }
+  try {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
 
-      const character = JSON.parse(body);
+    if (characters.length === 0) {
+      console.log("No characters found for this movie.");
+    } else {
+      characters.forEach(async (characterUrl) => {
+        try {
+          const characterResponse = await new Promise((resolve, reject) => {
+            request(characterUrl, (error, response, body) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(body);
+              }
+            });
+          });
 
-      console.log(character.name);
-    });
-  });
+          const characterData = JSON.parse(characterResponse);
+          console.log(characterData.name);
+        } catch (parseError) {
+          console.error(`Error parsing character data: ${parseError.message}`);
+        }
+      });
+    }
+  } catch (parseError) {
+    console.error(`Error parsing JSON response: ${parseError.message}`);
+    process.exit(1);
+  }
 });
